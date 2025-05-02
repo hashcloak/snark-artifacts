f311a08bd90d4020aa0598ce5cfd0a95d95d9769
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2025-05-02 16:47:23 -0600

    Revert "Add updated and new noir related artifacts"
    This reverts commit fbcd337296922892c773cc65a7dbe2b26525cbfe.

--- packages/artifacts/src/index.node.ts
+++ packages/artifacts/src/index.node.ts
@@ -1,18 +1,5 @@
 export { download, maybeDownload } from './download/download'
 export * from './index.shared'
-import maybeGetSnarkArtifacts, {
-  BatchingCircuitType,
-  getCompiledBatchCircuitWithPath,
-  getCompiledNoirCircuitWithPath,
-  maybeGetBatchVkPath,
-} from './download/index.node'
+import maybeGetSnarkArtifacts from './download/index.node'
 import { maybeGetCompiledNoirCircuit, maybeGetNoirVk } from './download/index.node'
-export {
-  BatchingCircuitType,
-  getCompiledBatchCircuitWithPath,
-  getCompiledNoirCircuitWithPath,
-  maybeGetBatchVkPath,
-  maybeGetCompiledNoirCircuit,
-  maybeGetNoirVk,
-  maybeGetSnarkArtifacts,
-}
+export { maybeGetCompiledNoirCircuit, maybeGetNoirVk, maybeGetSnarkArtifacts }
